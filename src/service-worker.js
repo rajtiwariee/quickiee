dc0f6e9f83905fb2c7d89e86636ef9ca978e5ba6
Raj S Tiwari <austiekbly@gmail.com>
2023-04-04 15:08:24 +0530

    done

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,42 +1,52 @@
-import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
+// Define the cache name and URLs to cache
+const CACHE_NAME = "ecommerce-pwa-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/images/logo.png",
+  "/images/products/product1.jpg",
+  "/images/products/product2.jpg",
+  "/images/products/product3.jpg",
+];
 
-precacheAndRoute(self.__WB_MANIFEST);
-
-if (workbox) {
-  console.log(`Workbox is loaded`);
-
-  // Precache all assets
-  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-  // Serve the precached assets first
-  workbox.routing.setDefaultHandler(
-    new workbox.strategies.CacheFirst({
-      cacheName: "assets-cache",
+// Install the service worker and cache the app shell
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
+});
 
-  // Serve the API requests with a network-first strategy
-  workbox.routing.registerRoute(
-    new RegExp("https://example.com/api/.*"),
-    new workbox.strategies.NetworkFirst({
-      cacheName: "api-cache",
+// Serve the app shell from the cache if available
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        console.log("Found in cache", event.request.url);
+        return response;
+      }
+      console.log("Not found in cache", event.request.url);
+      return fetch(event.request);
     })
   );
+});
 
-  // Serve the pages with a network-first strategy
-  workbox.routing.registerRoute(
-    new RegExp("https://example.com/.*"),
-    new workbox.strategies.NetworkFirst({
-      cacheName: "pages-cache",
+// Update the service worker and delete old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
-
-  // Listen for updates to the service worker
-  self.addEventListener("install", (event) => {
-    event.waitUntil(self.skipWaiting());
-  });
-
-  self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim());
-  });
-}
+});
