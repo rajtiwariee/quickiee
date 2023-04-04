32b7f489c34608752fed9f73b622742e6df5a4ac
Raj S Tiwari <austiekbly@gmail.com>
2023-04-04 15:20:31 +0530

    done

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,66 +1,52 @@
-// Define the cache name
-const CACHE_NAME = "my-ecommerce-pwa-cache-v1";
-
-// Define the files to cache
-const urlsToCache = [
+// Define the cache name and URLs to cache
+const CACHE_NAME = "ecommerce-pwa-cache-v1";
+const URLS_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.json",
   "/css/style.css",
   "/js/main.js",
   "/images/logo.png",
-  "/images/icons/icon-72x72.png",
-  "/images/icons/icon-96x96.png",
-  "/images/icons/icon-128x128.png",
-  "/images/icons/icon-144x144.png",
-  "/images/icons/icon-152x152.png",
-  "/images/icons/icon-192x192.png",
-  "/images/icons/icon-384x384.png",
-  "/images/icons/icon-512x512.png",
+  "/images/products/product1.jpg",
+  "/images/products/product2.jpg",
+  "/images/products/product3.jpg",
 ];
 
-// Install the service worker
-this.addEventListener("install", (event) => {
-  console.log("Service worker installed");
-
+// Install the service worker and cache the app shell
+window.self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Cache opened");
-      return cache.addAll(urlsToCache);
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-// Activate the service worker
-this.addEventListener("activate", (event) => {
-  console.log("Service worker activated");
+// Serve the app shell from the cache if available
+window.self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        console.log("Found in cache", event.request.url);
+        return response;
+      }
+      console.log("Not found in cache", event.request.url);
+      return fetch(event.request);
+    })
+  );
+});
 
+// Update the service worker and delete old caches
+window.self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache", cacheName);
             return caches.delete(cacheName);
           }
         })
       );
-    })
-  );
-});
-
-// Fetch the cached files
-this.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        console.log("Cache hit:", event.request.url);
-        return response;
-      }
-
-      console.log("Cache miss:", event.request.url);
-      return fetch(event.request);
     })
   );
 });
